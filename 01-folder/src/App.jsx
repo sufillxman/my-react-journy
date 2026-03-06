9f11acbcf80bd25d17298d1a3d6acab1f70bf726
Sufill X Man <sufillxman@gmail.com>
2026-03-07 03:04:17 +0530

    01_folder start

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function App() {
+  return (
+  <>
+  <div id="dad">
+    <h1>heloo</h1>
+  </div>
+  <div id="chacha">
+    <h2>
+      hello 2
+    </h2>
+  </div>
+  </>
+  )
+}
+
+export default App
---
+++ app.js
@@ -0,0 +1,4 @@
+ export const a= 10
+const arr=[10,20,30,40,50]
+
+export default arr
