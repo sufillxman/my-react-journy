b426b024f5dc4efcb373e9bd592c4fb32512cc69
Sufill X Man <sufillxman@gmail.com>
2026-03-11 03:53:50 +0530

    local storage chapter is complate

---
+++ 13-local-storage/src/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { jsx } from "react/jsx-runtime";
+
+const App = () => {
+  // localStorage.setItem('user','sufill')
+
+  // console.log(localStorage.getItem('user'));
+
+  // localStorage.removeItem('user')
+
+  // localStorage.clear()
+
+  // const user = {
+  //   username: "sufill",
+  //   age: 21,
+  //   city: "mumanvas",
+  // };
+  // console.log(user)
+
+  // localStorage.setItem('user',user)
+
+  // JSON.stringify ye localStorage me save kar va ne ke liye hai isse pura object vesa ka vesa save hoga
+  // localStorage.setItem('user',JSON.stringify(user))
+
+  // JSON.parse ye jab data ko nikal na ho to us ko vapas object me convert kar ne ke liye kam kar ta hai
+  // console.log(JSON.parse(localStorage.getItem('user')));
+
+  return <div>App</div>;
+};
+
+export default App;
